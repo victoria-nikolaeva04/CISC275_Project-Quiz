7587884d0707d5f58321f85398c3a8a72c72805c
Dustine Trieu <dtrieu7679@gmail.com>
2024-04-04 15:27:34 -0400

    ClearButton placeholder

---
+++ src/Components/ClearButton.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+export function ClearButton(): JSX.Element{
+    
+    return<div>
+        <Button onClick ={() => console.log("I am logged")}>Clear</Button>
+    </div>; 
+}
