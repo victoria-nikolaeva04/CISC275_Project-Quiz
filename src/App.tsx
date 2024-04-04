92fa443780e59fa4dc65b8e6559fc002542772de
Dustin Tran <dustintr@udel.edu>
2024-04-04 10:37:36 -0400

    add-name-dustintran1324

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
         <br></br>
         <p>
           Team Manager: Victoria Nikolaeva
-          Team Members: Giovanna Scozzaro, Dustine Trieu, Dustin Tran
+          Team Members: Giovanna Scozzaro, Dustine Trieu, Duy Duc Tran
         </p>
       </header>
       <Form>
