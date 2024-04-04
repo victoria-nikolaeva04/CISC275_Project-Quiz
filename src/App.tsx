ed32606e4f7603a780c43faec8582a34dc499d7c
Dustine Trieu <dtrieu7679@gmail.com>
2024-04-04 19:35:36 -0400

    CLearButton placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
+import { ClearButton } from './Components/ClearButton';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -39,6 +40,8 @@ function App() {
         >
           Learn React
         </a>
+        <hr></hr>
+        <ClearButton></ClearButton>
         <hr></hr>
         <br></br>
         <p>
--- src/Components/BasicNavigate.tsx
+++ src/Components/BasicNavigate.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
-
 export function ClearButton(): JSX.Element{
-    const [text, setText] = useState("");
-
     return<div>
-        <button onClick={() => setText("")}>Clear</button>
+        <Button onClick ={() => console.log("I am logged")}>Clear</Button>
     </div>; 
 }
