2fe89e8c0a9cc8fdc841545093d5f5541a923920
g33ya <gscozzaro2004@gmail.com>
2024-04-05 01:38:34 -0400

    changedetailedquestion.tsx progress - g33ya

--- src/ChangeDetailedQuestion.tsx
+++ src/ChangeDetailedQuestion.tsx
@@ -1,28 +1,12 @@
 import React, { useState } from "react";
 import { Button, Container, Row , Col, Form} from "react-bootstrap";
+import './App.css'; // need to make a sep file for this
+
 
 export function ChangeDetailedQuestion(): JSX.Element {
     //const [detailedQuestion, changePrompt] = useState<string>("");
 
-    /*
-    function changeQuestion(questionNumber: number){
-        if(questionNumber === 1){
-            
-        } else if (questionNumber === 2){
-
-        } else if (questionNumber === 3){
-
-        } else if (questionNumber === 4){
-
-        } else if (questionNumber === 5){
-
-        } else if (questionNumber === 6){
-
-        } else {
-
-        }
-    }
-    */
+    
     return (
         <span>
             <div className="Question-row">
