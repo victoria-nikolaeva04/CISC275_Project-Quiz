6d95d32a6ca568a444d7e9adeaac18cb3d3935b3
g33ya <gscozzaro2004@gmail.com>
2024-04-05 01:18:26 -0400

    Moved HTML design into ChangeDetailedQuestion - g33ya

--- src/ChangeDetailedQuestion.tsx
+++ src/ChangeDetailedQuestion.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Container, Row , Col, Form} from "react-bootstrap";
 
-export function Counter(): JSX.Element {
-    const [prompt, changePrompt] = useState<string>("");
+export function ChangeDetailedQuestion(): JSX.Element {
+    //const [detailedQuestion, changePrompt] = useState<string>("");
 
+    /*
     function changeQuestion(questionNumber: number){
         if(questionNumber === 1){
             
@@ -21,13 +22,67 @@ export function Counter(): JSX.Element {
 
         }
     }
+    */
     return (
         <span>
-            <div>
-                <h3>Counter</h3>
+            <div className="Question-row">
+                <Container>
+                <Row className="Horizontal-questions">
+                <Col className="Question-1" xs={2}>
+                    <Button>1</Button>
+                    </Col>
+                    <Col className="Question-2" xs={2}>
+                    <Button>2</Button>
+                    </Col>
+                    <Col className="Question-3" xs={2}>
+                    <Button>3</Button>
+                    </Col>
+                    <Col className="Question-4" xs={2}>
+                    <Button>4</Button>
+                    </Col>
+                    <Col className="Question-5" xs={2}>
+                    <Button>5</Button>
+                    </Col>
+                    <Col className="Question-6" xs={2}>
+                    <Button>6</Button>
+                    </Col>
+                    <Col className="Question-7" xs={2}>
+                    <Button>7</Button>
+                    </Col>
+                </Row>
+                </Container>
             </div>
-            <Button onClick={() => setValue(1 + value)}>Add One</Button>
-            to {value}.
+
+            <div className="Question-textbox">
+                <p>Enter question text here</p>
+            </div>
+
+            <div className="Answer-choices">
+                    <Container className="Extend-width">
+                    <Row className="Answer-row">
+                        <Form.Check
+                            type="radio"
+                            name="emotions"
+                            //onChange={updateEmotion}
+                            id="emotion-check-happy"
+                            label="Answer Choice 1"
+                            value="happy"
+                            inline
+                            //checked={emotion === "happy"}
+                        />
+                        <Form.Check
+                            type="radio"
+                            name="emotions"
+                            //onChange={updateEmotion}
+                            id="emotion-check-sad"
+                            label="Answer Choice 2"
+                            value="sad"
+                            inline
+                            //checked={emotion === "sad"}
+                        />
+                    </Row>
+                    </Container>       
+            </div>
         </span>
     );
 }
