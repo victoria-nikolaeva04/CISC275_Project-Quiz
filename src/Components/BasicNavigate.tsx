25476c0e4edbb62b15bb39c85d5e60b4b070b9aa
Dustine Trieu <dtrieu7679@gmail.com>
2024-04-09 14:40:08 -0400

    Button

--- src/Components/BasicNavigate.tsx
+++ src/Components/BasicNavigate.tsx
@@ -1,16 +1,13 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
-export function ClearButton(): JSX.Element{
+export function BasicButton(): JSX.Element{
     const [buttonClick, setClick]= useState<boolean>(false);
 
     const handleEditModeChange = () => {
         setClick(!buttonClick);
     }
     return<div>
-        <h3>Basic Questions</h3>
-        <Form.Check>
-        </Form.Check>
         <Button onClick ={() => console.log("I am logged")}>Basic Questions</Button>
     </div>; 
 }
--- src/Components/DetailNaviButton.tsx
+++ src/Components/DetailNaviButton.tsx
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
-export function ClearButton(): JSX.Element{
+export function DetailedButton(): JSX.Element{
+    const [buttonClick, setClick]= useState<boolean>(false);
+
+    const handleEditModeChange = () => {
+        setClick(!buttonClick);
+    }
     return<div>
-        <Button onClick ={() => console.log("I am logged")}>Clear</Button>
+        <Button onClick ={() => console.log("I am logged")}>Detailed Questions</Button>
     </div>; 
 }
