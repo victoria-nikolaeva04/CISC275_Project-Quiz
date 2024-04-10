854bcd960ad3927f63bbc4088fb057f1144d30ae
Dustine Trieu <dtrieu7679@gmail.com>
2024-04-10 17:33:14 -0400

    Link button test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
 import  ClearButton  from './Components/ClearButton';
+import LinkButton from './Components/BasicNavigate'
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -41,7 +42,8 @@ function App() {
           Learn React
         </a>
         <hr></hr>
-        {<ClearButton></ClearButton>}
+        {/*<ClearButton></ClearButton>*/}
+        {/*<LinkButton to={''} className={''}>Children Text Here</LinkButton>*/}
         <hr></hr>
         <br></br>
         <p>
--- src/Components/BasicNavigate.tsx
+++ src/Components/BasicNavigate.tsx
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
-import { HashRouter as Router, Route, Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export function BasicButton(): JSX.Element{
-
-    function ButtonLink({ to, children }) {
-        return <Link to={to}><button>{children}</button></Link>;
-        // to prop = the link destination
-        // children prop = button text
-      }
-
-    return<div>
-        <ButtonLink to = ""> Basic Questions</ButtonLink>
-    </div>; 
+interface LinkButtonProps{
+    to:string;
+    children:string;
+    className:string;
 }
+
+const ButtonLink = (props:LinkButtonProps) => {
+  return (
+    <Link to={props.to} className={props.className}>
+      <button>{props.children}</button>
+    </Link>
+  );
+};
+
+export default ButtonLink;
