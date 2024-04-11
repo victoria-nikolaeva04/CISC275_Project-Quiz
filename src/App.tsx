add3e6bd0010e8cc06894752eb87d49a73052119
g33ya <gscozzaro2004@gmail.com>
2024-04-11 19:41:21 -0400

    Matching App.tsx before push - g33ya

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Button, Form } from 'react-bootstrap';
-import { DetailedQuestions } from "./DetailedQuestions";
+import ClearButton from './Components/ClearButton';
+import ButtonLink from './Components/ButtonLink';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and other necessary components
+import Questionnaire from './basicQuestion';
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -25,42 +28,48 @@ function App() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router> 
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <hr></hr>
+          {/*<ClearButton></ClearButton>*/}
+          {/*<ButtonLink to={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'} className={'My-button'}>Home Button</ButtonLink>*/}
+          {/*<ButtonLink to={'/'} className={'My-button'}>Detail Question</ButtonLink>*/}
+          {/*<ButtonLink to={'/Pages/BasicQuestion'} className={'My-button'}>Basic Question</ButtonLink>*/}
 
-        <hr></hr>
-        <br></br>
-        <p>
-          Team Manager: Victoria Nikolaeva
-          Team Members: Giovanna Scozzaro, Dustine Trieu, Duy Duc Tran
-        </p>
-      </header>
-      <Form>
-        <Form.Label>API Key:</Form.Label>
-        <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-        <br></br>
-        <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-      </Form>
-
-      <div className="Answer-choices">
-      <DetailedQuestions></DetailedQuestions>
+          <hr></hr>
+          <br></br>
+          <p>
+            Team Manager: Victoria Nikolaeva
+            Team Members: Giovanna Scozzaro, Dustine Trieu, Duy Duc Tran
+          </p>
+        </header>
+        <Routes>
+          <Route path="/basicQuestion" element={<Questionnaire />} />
+          {/* Add more routes as needed */}
+          </Routes>
+        <Form>
+          <Form.Label>API Key:</Form.Label>
+          <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
+          <br></br>
+          <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
+        </Form>
       </div>
-    </div>
-
-    
+    </Router>
   );
 }
 
