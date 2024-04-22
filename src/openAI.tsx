44e0bef290ad0dd14d981646d55cdd59a108682a
Dustine Trieu <dtrieu7679@gmail.com>
2024-04-22 12:55:17 -0400

    Commiting stuff

--- src/openAI.tsx
+++ src/openAI.tsx
@@ -3,12 +3,8 @@ import OpenAI from "openai";
 const openai = new OpenAI();
 
 async function main() {
-  const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
-  });
+  const image = await openai.images.generate({ prompt: "A cute baby sea otter" });
 
-  console.log(completion.choices[0]);
+  console.log(image.data);
 }
-
 main();
