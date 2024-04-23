c99218e006ff347c2c98e4615164d1f837c6e560
Victoria Nikolaeva <vniko@udel.edu>
2024-04-23 03:14:32 -0400

    Creating Footer Component

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import logo  from "./images/CISC-Logo.png";
-import simple_logo  from "./images/CISC-Footer-Logo.png";
-import github from "./images/CISC-Footer-GIT.png";
 import temp from "./images/temp_image.png";
 import './AboutMe.css';
 import { Button, Form } from 'react-bootstrap';
--- src/homepage.tsx
+++ src/homepage.tsx
@@ -19,19 +19,6 @@ if (prevKey !== null) {
 }
 
 function Homepage() {
-  const [key, setKey] = useState<string>(keyData); //for api key input
-  
-  //sets the local storage item to the api key the user inputed
-  function handleSubmit() {
-    localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
-  }
-
-  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
-  function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
-    setKey(event.target.value);
-  }
-  return (
     <><div className="App">
       {/*
       <header className="App-header">
@@ -109,39 +96,12 @@ function Homepage() {
         <br></br>
       </body>
 
-      <footer>
-        <div className="footer_decorative">
-          <div id="logo-text-footer">
-              <img
-                src={simple_logo}
-                alt="Hard Pic"
-                className ="footer-logo"
-              />
-               <p className="team-name-footer">@SWE TEAM</p>
-          </div>
-          <a href='https://github.com/VikiNiki123/starter_helpi' className="contact-info-footer">
-            <img
-                    src={github}
-                    alt="GitHub"
-                    className ="footer-logo"
-                />
-          </a>
-        </div>
-        <div className="API_footer">
-          <Form>
-            <Form.Label>API Key: </Form.Label>
-            <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
-            <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
-          </Form>
-        </div>
-
-      </footer>
+      
         {/*
            <Link to="/basic-quiz"><Button>Basic Quiz</Button></Link>
            <Routes><Route path="/basic-quiz" element={<Questionnaire />} /></Routes>
         */}
         <br></br>
     </div></>
-  );
 }
 export default Homepage;
