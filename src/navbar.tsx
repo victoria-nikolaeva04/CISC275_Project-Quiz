0d63a27977f5e49f17f462189d2bd81c696360ed
Victoria Nikolaeva <vniko@udel.edu>
2024-04-24 15:48:24 -0400

    Footer and Header work

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -11,31 +11,22 @@ export function CollapsibleNavbar() {
   return (
     <Navbar collapseOnSelect expand="lg" id = "vertical" className="bg-body-tertiary" style={{color: '#0f14b7'}}>
       <Container id="container">
-        <div className="cat-header-img">
-          <img
-              src={catHeader} 
-              alt="cat-header"
-              id="cat-header"
-          />
-          <Navbar.Brand as={Link} to="/homepage">Cat-ylist Career</Navbar.Brand>
-        </div>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand as={Link} to="/homepage">Cat-ylist Career</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/BasicQuestion">Basic Question</Nav.Link>
             <Nav.Link as={Link} to="/DetailedQuestion">Detail Question</Nav.Link>
             <Nav.Link as={Link} to="/AboutMe">About Me</Nav.Link>
           </Nav>
-          <Nav>
-          </Nav>
-        </Navbar.Collapse>   
-        <Nav.Item className='navbar-item'>
+          <Nav.Item className='navbar-logo'>
               <img
                 src={logo} 
                 alt="logo"
                 id="logo-pic"
               />
-      </Nav.Item>
+        </Nav.Item>
+        </Navbar.Collapse>   
       </Container>
     </Navbar>
   );
