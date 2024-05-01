c488672b937ab6e09abe74d1e6bacc469af8a660
Dustin Tran <dustintr@udel.edu>
2024-04-30 21:00:10 -0400

    Adding description for basicQuestion and fixed the useLocation issue for the result page

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Homepage from './homepage';
-import CollapsibleNavbar from './navbar'
+import CollapsibleNavbar from './navbar';
 import Questionnaire from './basicQuestion';
-import { BrowserRouter as HashRouter,Routes, Route} from 'react-router-dom'; // Import BrowserRouter and other necessary components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter and other necessary components
 import BasicResult from './basicresult';
 import DetailedQuestions from './DetailedQuestions';
 import AboutMe from './AboutMe';
 import Result from './result';
-import Footer from './footer'
+import Footer from './footer';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <HashRouter>
-  <CollapsibleNavbar></CollapsibleNavbar>
-     <Routes>
-          <Route path="/" element={<Homepage />} /> 
-          <Route path="/basicQuestion" element={<Questionnaire />} />
-          <Route path="/App" element={<App/>} />
-          <Route path="/DetailedQuestion" element={<DetailedQuestions/>} />
-          <Route path="/basicresult" element={<BasicResult/>} />
-          <Route path="/homepage" element={<Homepage/>} />
-          <Route path="/AboutMe" element={<AboutMe/>} />
-          <Route path="/result" element={<Result/>} />
-          {/* Add more routes as needed */}
-          </Routes>
-          <Footer></Footer>
-  <React.StrictMode>
-  </React.StrictMode>
-  </HashRouter>
-  
+ReactDOM.render(
+  <Router>
+    <CollapsibleNavbar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/basicQuestion" element={<Questionnaire />} />
+      <Route path="/App" element={<App />} />
+      <Route path="/DetailedQuestion" element={<DetailedQuestions />} />
+      <Route path="/basicresult" element={<BasicResult />} /> {}
+      <Route path="/homepage" element={<Homepage />} />
+      <Route path="/AboutMe" element={<AboutMe />} />
+      <Route path="/result" element={<Result />} />
+      {/* Add more routes as needed */}
+    </Routes>
+    <Footer />
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a brick
