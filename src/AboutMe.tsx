b213b8a363eb4dee38d3cd8b5e7af855f7375eda
Victoria Nikolaeva <vniko@udel.edu>
2024-05-09 04:26:32 -0400

    Update Information on About Me Page

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -4,6 +4,7 @@ import VikiPhoto from'./images/Viki-Photo.png';
 import GiaPhoto from'./images/Gia-Photo.png';
 import DustinPhoto from'./images/Dustin-Photo.png';
 import DustinePhoto from'./images/Dustine-Photo.png';
+import linkinLogo from './images/linkin-logo.png';
 
 
 const AboutMe: React.FC = () => {
@@ -15,14 +16,13 @@ const AboutMe: React.FC = () => {
         <div className="team-container">
           <h3 className="AboutMe-heading">Meet the Team!</h3>
           <p className="AboutMe-text">
-            A cohesive and dynamic four-person team can be a powerful force within an organization. 
-            Each member brings unique skills, experiences, and perspectives to the table, contributing 
-            to the team's overall success. With clear communication channels and a shared vision, they 
-            collaborate efficiently, leveraging their individual strengths to tackle complex challenges 
-            and achieve collective goals. The team's diversity fosters creativity and innovation, leading
-            to well-rounded solutions and continuous improvement. Through mutual support, trust, and 
-            effective leadership, this team cultivates a positive work environment where everyone feels 
-            valued and motivated, paving the way for sustained growth and success."
+            HEY, Welcome to Cat-lyis Quiz!To get to know te site a little bit better and to have a face behind the site, meet the team of
+            young developers from Univeristy of Delaware. This site is the hardwork of our final project for our 275 - Intro to Software Development.
+            Together, we have worked and learn many things, not only as programmers, but as team. We learned to manage, communicate, and resolve issues
+            with both within ourselves but also with our 'client' (aka our fun Professor Armanando). This project, isn't about the code itself, but the 
+            processes and thinking that leads us to implementing. We are not code-monkeys, we are engineers (even if UD hardly recognize that). Our team
+            is full of strong minds and creative thinkers, leading to our well-rounded solutions and continous improvement. Even if our journey started off
+            shakey, by the end we have become solid as gold. Hopeful, you will see the work we put into the site.<br></br> Thank you!
           </p>
         </div>
         <div className="card-collection">
@@ -36,9 +36,20 @@ const AboutMe: React.FC = () => {
               />
               <div className="container-card">
                 <h2>Victoria</h2>
-                  <p className="title-position">Manager</p>
-                  <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-                  <p>vniko@udel.edu</p>
+                  <p className="title-position">(Wo)Man with a Plan</p>
+                  <p>Manager of the team! Worked on the Home & About Us Pages and created design guides for the team and many of the assets. Little Miss Artist!</p>
+                  <div className='contact-box'>
+                      <p>vniko@udel.edu</p>
+                      <a href="https://www.linkedin.com/in/victoria-nikolaeva-447011258/">
+                        <img 
+                            src={linkinLogo} 
+                            alt="linkin-logo"
+                            className='linkin-logo'
+                        />
+                      </a>
+                  </div>
+                  
+
               </div>
             </div>
         </div>
@@ -52,9 +63,19 @@ const AboutMe: React.FC = () => {
             />
             <div className="container-card">
               <h2>Dustin</h2>
-              <p className="title-position">The Fruit in the Basket</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>example@example.com</p>
+              <p className="title-position">Canvas Connoisseu</p>
+              <p>Work on the Basic Career Quiz Layout and the Result Page Layout. He helped with Design Guides with the creativity flowing through his fingers!</p>
+              <div className='contact-box'>
+                      <p>dustintr@udel.edu</p>
+                      <a href="https://www.linkedin.com/in/duy-duc-tran/">
+                        <img 
+                            src={linkinLogo} 
+                            alt="linkin-logo"
+                            className='linkin-logo'
+                        />
+                      </a>
+                  </div>
+              
             </div>
           </div>
         </div>
@@ -69,8 +90,17 @@ const AboutMe: React.FC = () => {
             <div className="container-card">
               <h2>Gia</h2>
               <p className="title-position">A Jack of All-Traits!</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>example@example.com</p>
+              <p>Worked on the Detailed Questions Page. She was a super-STAR with design and functionailty details! She was everywhere!</p>
+              <div className='contact-box'>
+                      <p>gscoz@udel.edu</p>
+                      <a href="https://www.linkedin.com/in/gscozzaro/">
+                        <img 
+                            src={linkinLogo} 
+                            alt="linkin-logo"
+                            className='linkin-logo'
+                        />
+                      </a>
+                  </div>
             </div>
           </div>
         </div>
@@ -85,8 +115,17 @@ const AboutMe: React.FC = () => {
             <div className="container-card">
               <h2>Dustine</h2>
               <p className="title-position">IT Department</p>
-              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p>example@example.com</p>
+              <p>He worked on the router and most of the API/AI handling. He handled most of the heavy backend with ease. He deserves applause!</p>
+              <div className='contact-box'>
+                      <p>dtrieu@udel.edu</p>
+                      <a href="https://www.linkedin.com/in/dustine-trieu/">
+                        <img 
+                            src={linkinLogo} 
+                            alt="linkin-logo"
+                            className='linkin-logo'
+                        />
+                      </a>
+                  </div>
             </div>
             </div>
           </div>
