ae51a46f8e15fd88b1343505e1dbc297182e22d6
Dustine Trieu <dtrieu7679@gmail.com>
2024-05-09 23:05:25 -0400

    fixed result formatting

--- src/basicresult.tsx
+++ src/basicresult.tsx
@@ -6,8 +6,19 @@ import './basicresult.css';
 
 function BasicResult() {
   const location = useLocation(); // Get the location object
-  const result = location.state?.result; // Retrieve the result from the location state
-
+  const { result} = location.state;
+  const formattedResult = result.split('\n').map(
+    (
+      line: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined,
+      index: React.Key | null | undefined
+    ) => {
+      if (line == null) {
+        return null; 
+      }
+      const cleanLine = line.toString().replace(/\*/g, ''); // Remove asterisks from the line
+      return <p key={index}>{cleanLine}</p>;
+    }
+  );
   return (
     <div>
       <Container className="job-and-salary-area">
@@ -16,7 +27,7 @@ function BasicResult() {
               <Card.Title id ="title">Result analysis</Card.Title>
               <Card.Text>
                 {/* Display the result received from the Questionnaire */}
-                <p className='result-text'>{result}</p>
+                <p className='result-text'>{formattedResult}</p>
               </Card.Text>
             </Card.Body>
           </Card>
